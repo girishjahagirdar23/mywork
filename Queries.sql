b5d4ae269b62c3b80eadf5819a876a88beed3f5b
Girish <girishjahagirdar23@gmail.com>
2026-05-20 15:53:57 +0530

    First version of file v1

---
+++ Queries.sql
@@ -0,0 +1,9 @@
+-- Query to display Floyed trainagle
+with ds as (
+select level s from dual connect by level <= 4
+)
+select listagg(rownum,' ') within group(order by c3) from (
+select rownum,a.s c2,b.s c3 from ds a,ds b
+where a.s >= b.s
+order by a.s,b.s)
+group by c2
